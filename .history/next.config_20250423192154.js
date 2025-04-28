a1529383b396f15b1cc09aba76ea45689d3dfe81
ShotaGhoona <shota.yamashita@ghoona.com>
2025-04-28 09:34:37 +0900

    今ある文を全部入れます

---
+++ .history/next.config_20250423192154.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'scontent-itm1-1.cdninstagram.com',
+      'scontent.cdninstagram.com',
+      'scontent-nrt1-1.cdninstagram.com',
+      'scontent-nrt1-2.cdninstagram.com',
+      'scontent-itm1-2.cdninstagram.com',
+    ],
+  },
+}
+
+module.exports = nextConfig 
